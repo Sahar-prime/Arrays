2810758bbba994fa3cf8647728ad5eddf6b0b501
user <user@WIN-4KGTNI4DDSK>
2025-06-09 00:29:52 +0300

    Add project files.

--- Arrays/Sort.cpp
+++ Arrays/Sort.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+п»ї#include <iostream>
 using namespace std;
 
 //#define SORT
@@ -10,13 +10,13 @@ int main()
     int arr[] = { 5, 1, 7, 3, 4, 0, 6, 2, 8, 9 };
     int size = sizeof(arr) / sizeof(arr[0]);
 
-    // Сортировка пузырьком
+    // РЎРѕСЂС‚РёСЂРѕРІРєР° РїСѓР·С‹СЂСЊРєРѕРј
     for (int i = 0; i < size - 1; ++i) 
     {
         for (int j = 0; j < size - i - 1; ++j)
         {
             if (arr[j] > arr[j + 1]) {
-                // Обмен значениями
+                // РћР±РјРµРЅ Р·РЅР°С‡РµРЅРёСЏРјРё
                 int temp = arr[j];
                 arr[j] = arr[j + 1];
                 arr[j + 1] = temp;
@@ -24,8 +24,8 @@ int main()
         }
     }
 
-    // Вывод отсортированного массива
-    cout << "Отсортированный массив: ";
+    // Р’С‹РІРѕРґ РѕС‚СЃРѕСЂС‚РёСЂРѕРІР°РЅРЅРѕРіРѕ РјР°СЃСЃРёРІР°
+    cout << "РћС‚СЃРѕСЂС‚РёСЂРѕРІР°РЅРЅС‹Р№ РјР°СЃСЃРёРІ: ";
     for (int i = 0; i < size; ++i)
     {
         cout << arr[i] << " ";
