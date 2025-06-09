49e8a232d805a914c1dc6ba86fcf58e9c613bcfe
user <user@WIN-4KGTNI4DDSK>
2025-06-09 12:40:46 +0300

    Add project files.

--- UniqueRand/main.cpp
+++ UniqueRand/main.cpp
@@ -1,10 +1,9 @@
 ﻿#include <iostream>
 #include <cstdlib> // Для функций rand() и srand()
 #include <ctime>   // Для функции time()
-
-//#define RANDOM_NUMB
-
 using namespace std;
+
+#define RANDOM_NUMB
 
 #ifdef RANDOM_NUMB
 int main()
@@ -17,13 +16,15 @@ int main()
     srand(time(0));
 
     // Заполнение массива случайными числами от 20 до 30
-    for (int i = 0; i < size; ++i) {
+    for (int i = 0; i < size; ++i)
+    {
         arr[i] = rand() % 11 + 20; // rand() % 11 дает числа от 0 до 10, поэтому прибавляем 20
     }
 
     // Вывод массива
     cout << "Массив из случайных чисел: ";
-    for (int i = 0; i < size; ++i) {
+    for (int i = 0; i < size; ++i)
+    {
         cout << arr[i] << " ";
     }
     cout << endl;
