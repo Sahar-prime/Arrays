c83bd97547fa2a45f9b8a46dc29cf601f7a1dc0b
user <user@WIN-4KGTNI4DDSK>
2025-06-12 15:20:56 +0300

    Add project files.

--- Numerics/main.cpp
+++ Numerics/main.cpp
@@ -1,20 +1,21 @@
 ﻿#include <iostream>
 using namespace std;
 
-#define TASK_1
+//#define TASK_1
 //#define TASK_2
 
-#define UROK
 //#define DZ
+//#define UROK
 
 #ifdef TASK_1
 int main()
 {
     setlocale(LC_ALL, "");
+#ifdef DZ
     int decimal;
     int binary[32]; // Массив для хранения двоичных цифр
 
-#ifdef DZ
+
     int index = 0;
 
     cout << "Введите десятичное число: ";
@@ -43,18 +44,17 @@ int main()
 #endif //DZ
 
 #ifdef UROK
-    const int MAX_HEX_CAPACITY = 8;
-    char hex[MAX_HEX_CAPACITY] = {};
-   
+    //for (int i = 0; i < 256; i++)		cout << i << "\t" << (char)i << endl;
+    int decimal;
+    cout << "Введите десятичное число: "; cin >> decimal;
+    const int CAPACITY = 32;	//Максимально возможная разрядность двоичного числа
+    bool bin[CAPACITY] = {};
     int i = 0;
-    for (; decimal; decimal /= 16) 
-    {
-        hex[i++] = decimal % 16 < 10 ? decimal % 16 + 48 : decimal % 16 + 55;
-        //hex[i++] = decimal % 16;
-        //hex[i++] += hex[i] < 10 ? 48 : 55;
-    }
-    for (--i; i >= 0; i--)cout << hex[i];
-    //for (--i; i >= 0; i--)cout << char(hex[i] < 10 ? hex[i] + 48 : hex[i] + 55);
+    for (; decimal; decimal /= 2)
+        bin[i++] = decimal % 2;
+
+    for (--i; i >= 0; i--)
+        cout << bin[i];
     cout << endl;
 #endif //UROK
 }
@@ -63,6 +63,7 @@ int main()
 #ifdef TASK_2
 int main()
 {
+#ifdef DZ
     setlocale(LC_ALL, "");
     long long decimalNumber;
     cout << "Введите десятичное число: ";
@@ -70,6 +71,25 @@ int main()
 
     // Переводим десятичное число в шестнадцатеричное
     cout << "Шестнадцатеричное представление: " << hex << decimalNumber << endl;
+#endif //DZ
+
+#ifdef UROK
+    int decimal;
+    cout << "Введите десятичное число: "; cin >> decimal;
+    const int MAX_HEX_CAPACITY = 8;
+    char hex[MAX_HEX_CAPACITY] = {};
+
+    int i = 0;
+    for (; decimal; decimal /= 16)
+    {
+        hex[i++] = decimal % 16 < 10 ? decimal % 16 + 48 : decimal % 16 + 55;
+        //hex[i] = decimal % 16;
+        //hex[i++] += hex[i] < 10 ? 48 : 55;
+    }
+    for (--i; i >= 0; i--)cout << hex[i];
+    //for (--i; i >= 0; i--)cout << char(hex[i] < 10 ? hex[i] + 48 : hex[i] + 55);
+    cout << endl;
+#endif //UROK
 }
 #endif //TASK_2
 
--- UniqueRand/main.cpp
+++ UniqueRand/main.cpp
@@ -33,28 +33,33 @@ int main()
 #endif //RANDOM_NUMB
 
 #ifdef RANDOM_NUMB_2
-void main() 
+void main()
 {
-    setlocale(LC_ALL, "");
-    const int n = 10;
-    int arr[n];
-    for (int i = 0; i < n; i++)
-    {
-        bool unique;
-        do
-        {
-            arr[i] = rand() % 10;
-            unique = true;
-            for (int j = 0; j < i; j++)
-            {
-                if (arr[i] == arr[j])
-                {
-                    unique = false;
-                    break;
-                }
-            }
-        } while (!unique);
-    }
+	setlocale(LC_ALL, "");
+	const int n = 10;
+	int arr[n];
+	for (int i = 0; i < n; i++)
+	{
+		bool unique;
+		do
+		{
+			arr[i] = rand() % 10;
+			unique = true;
+			for (int j = 0; j < i; j++)
+			{
+				if (arr[i] == arr[j])
+				{
+					unique = false;
+					break;
+				}
+			}
+		} while (!unique);
+	}
 
+	for (int i = 0; i < n; i++)
+	{
+		cout << arr[i] << "\t";
+	}
+	cout << endl;
 }
 #endif //RANDOM_NUMB_2
